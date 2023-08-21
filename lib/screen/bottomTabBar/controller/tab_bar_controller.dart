ac5db8af18cfdd823ed47f6d465d777acb1b32a9
reflexit2020 <robiulstudies@gmail.com>
2023-08-21 08:12:12 +0600

    Initial commit

---
+++ lib/screen/bottomTabBar/controller/tab_bar_controller.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:persistent_bottom_nav_bar/persistent_tab_view.dart';
+
+import '../page/home/homepage.dart';
+import '../page/inbox/inboxpage.dart';
+
+class TabNavvController extends GetxController {
+  PersistentTabController controller = PersistentTabController(initialIndex: 0);
+
+  List<Widget> buildScreens() {
+    return [
+      HomePage(),
+      InboxPage(),
+      InboxPage(),
+      HomePage(),
+      InboxPage(),
+    ];
+  }
+
+  List<PersistentBottomNavBarItem> navBarsItems() {
+    return [
+      PersistentBottomNavBarItem(
+        icon: Icon(CupertinoIcons.home),
+        title: ("Home"),
+        activeColorPrimary: CupertinoColors.activeOrange,
+        inactiveColorPrimary: CupertinoColors.systemGrey,
+      ),
+      PersistentBottomNavBarItem(
+        icon: Icon(Icons.flaky),
+        title: ("Explore"),
+        activeColorPrimary: CupertinoColors.activeOrange,
+        inactiveColorPrimary: CupertinoColors.systemGrey,
+      ),
+      PersistentBottomNavBarItem(
+        icon: Icon(CupertinoIcons.add,color: Colors.white,size: 40,),
+        activeColorPrimary: CupertinoColors.black,
+        inactiveColorPrimary: CupertinoColors.systemGrey,
+      ),
+      PersistentBottomNavBarItem(
+        icon: Icon(CupertinoIcons.mail),
+        title: ("Inbox"),
+        activeColorPrimary: CupertinoColors.activeOrange,
+        inactiveColorPrimary: CupertinoColors.systemGrey,
+      ),
+      PersistentBottomNavBarItem(
+        icon: Icon(CupertinoIcons.creditcard),
+        title: ("Shop"),
+        activeColorPrimary: CupertinoColors.activeOrange,
+        inactiveColorPrimary: CupertinoColors.systemGrey,
+      ),
+    ];
+  }
+}
---
+++ lib/utils/urls_and_app_constants.dart
@@ -0,0 +1,9 @@
+// ignore_for_file: unnecessary_string_interpolations, constant_identifier_names, duplicate_ignore
+
+class AppUrls {
+  // ignore: constant_identifier_names
+  static const String APP_NAME = 'PARASOL HEALTH';
+  static const String URL = 'https://mocki.io/v1/02e91d9f-4d8c-4eb4-a4f9-bb4a60ffdaf9';
+
+
+}
